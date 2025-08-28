72129ae60e705d816b32cc693eb07a88b8de6c98
Andrew K <andrew.katsewich@gmail.com>
2025-08-28 19:33:22 +0300

    chore: move examples to apps/ workspace as part of monorepo

---
+++ apps/examples/svelte/src/lib/ImageZoom.svelte
@@ -0,0 +1,33 @@
+<script>
+import mediumZoom from 'medium-zoom';
+
+export const src = undefined;
+export const alt = undefined;
+export const options = undefined;
+
+let zoom = null;
+
+function getZoom() {
+	if (zoom === null) {
+		zoom = mediumZoom(options);
+	}
+
+	return zoom;
+}
+
+function attachZoom(image) {
+	const zoom = getZoom();
+	zoom.attach(image);
+
+	return {
+		update(newOptions) {
+			zoom.update(newOptions);
+		},
+		destroy() {
+			zoom.detach();
+		},
+	};
+}
+</script>
+
+<img {src} {alt} {...$$restProps} use:attachZoom={options} />
